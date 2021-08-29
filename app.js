2d421aaf8cfe672025fc70faa50ffa9343092281
yitongknows <61811966+yitongknows@users.noreply.github.com>
2021-08-28 23:30:28 -0400

    added prettier formatting

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const ExpressError = require('./utils/ExpressError')
-const ejsMate = require('ejs-mate')
-const session = require('express-session');
-const flash = require('connect-flash')
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
-const User = require('./models/user')
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ExpressError = require("./utils/ExpressError");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const campgroundsRoutes = require("./routes/campgrounds");
+const reviewsRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
+const User = require("./models/user");
 
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
 });
 
 const db = mongoose.connection;
@@ -28,64 +28,64 @@ db.once("open", () => {
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.engine('ejs', ejsMate)
-
+app.engine("ejs", ejsMate);
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
+    secret: "thisshouldbeabettersecret!",
     resave: false,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         httpOnly: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: Date.now() + 1000 * 60 * 60 * 24 * 7
-    }
-}
-app.use(session(sessionConfig))
-app.use(flash())
+        maxAge: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    },
+};
+app.use(session(sessionConfig));
+app.use(flash());
 // make sure app.use(session()) is before the passport.session()
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()))
+passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    res.locals.success = req.flash('success')
-    res.locals.error = req.flash('error')
-    next()
-})
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use("/campgrounds", campgroundsRoutes);
+app.use("/campgrounds/:id/reviews", reviewsRoutes);
+app.use("/", userRoutes);
 
-app.get('/', (req, res) => {
-    res.render('campgrounds/index');
-})
+app.get("/", (req, res) => {
+    res.render("campgrounds/index");
+});
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({email: 'apple@gmail.com', username: 'apple'})
-    const newUser = await User.register(user, 'chicken');
+app.get("/fakeUser", async (req, res) => {
+    const user = new User({ email: "apple@gmail.com", username: "apple" });
+    const newUser = await User.register(user, "chicken");
     res.send(newUser);
-})
+});
 
-app.all('*', (req, res, next)=>{
-    next(new ExpressError('Page Not Found', 404))
-})
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404));
+});
 
-app.use((err, req, res, next) =>{
-    const {statusCode = 500} = err;
-    if(!err.message) err.message = "Oh no, something went wrong"
-    res.status(statusCode).render('error', {err})
-})
-app.listen(3000, () =>{
-    console.log('Serving on port 3000');
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = "Oh no, something went wrong";
+    res.status(statusCode).render("error", { err });
+});
+app.listen(3000, () => {
+    console.log("Serving on port 3000");
 });
