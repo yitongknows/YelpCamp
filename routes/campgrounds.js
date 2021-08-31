f0c76a95e70e235b06931acbba6090df911ce65b
yitongknows <61811966+yitongknows@users.noreply.github.com>
2021-08-31 01:08:52 -0400

    use express.router

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,39 +5,33 @@ const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const campgrounds = require("../controllers/campgrounds");
 const router = express.Router();
 
-router.get("/", catchAsync(campgrounds.index));
-
+router
+    .route("/")
+    .get(catchAsync(campgrounds.index))
+    .post(
+        isLoggedIn,
+        validateCampground,
+        catchAsync(campgrounds.createCampground)
+    );
+//this needs to go before the /:id otherwise it will think new is an id
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post(
-    "/",
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-);
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router
+    .route("/:id")
+    .get(catchAsync(campgrounds.showCampground))
+    .put(
+        isLoggedIn,
+        isAuthor,
+        validateCampground,
+        catchAsync(campgrounds.updateCampground)
+    )
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get(
     "/:id/edit",
     isLoggedIn,
     isAuthor,
     catchAsync(campgrounds.renderEditForm)
-);
-
-router.put(
-    "/:id",
-    isLoggedIn,
-    isAuthor,
-    validateCampground,
-    catchAsync(campgrounds.updateCampground)
-);
-
-router.delete(
-    "/:id",
-    isLoggedIn,
-    isAuthor,
-    catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
